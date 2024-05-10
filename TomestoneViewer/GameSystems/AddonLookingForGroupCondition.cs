6decc717b518a5739178f47c2e85a0abd3fd97a3
anomek <149334678+anomek@users.noreply.github.com>
2024-05-10 00:31:54 -0700

    Autodetect encounter when creating party

---
+++ TomestoneViewer/GameSystems/AddonLookingForGroupCondition.cs
@@ -0,0 +1,50 @@
+using System.Runtime.InteropServices;
+
+using FFXIVClientStructs.Attributes;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+
+#pragma warning disable SA1134 // Attributes should not share line
+namespace TomestoneViewer.GameSystems;
+
+[Addon("LookingForGroupCondition")]
+[StructLayout(LayoutKind.Explicit, Size = 0x354)]
+public unsafe partial struct AddonLookingForGroupCondition
+{
+    [FieldOffset(0)] public AtkUnitBase AtkUnitBase;
+
+    [FieldOffset(0x220)] public AtkComponentDropDownList* DutyCategoryDropDown;
+    [FieldOffset(0x228)] public AtkComponentDropDownList* DutyNameDropDown;
+    [FieldOffset(0x230)] public AtkComponentTextInput* CommentInput;
+
+    [FieldOffset(0x238)] public AtkComponentNumericInput* PartyPasswordInput;
+
+    [FieldOffset(0x240)] public AtkComponentCheckBox* UnselectClassesCheckBox;
+    [FieldOffset(0x248)] public AtkComponentCheckBox* FormPrivatePartyCheckBox;
+    [FieldOffset(0x250)] public AtkComponentCheckBox* LimitRecruitingToWorldServerCheckBox;
+    [FieldOffset(0x258)] public AtkComponentCheckBox* OnePlayerPerJobCheckBox;
+
+    [FieldOffset(0x260)] public AtkComponentCheckBox* AvgItemLevelCheckBox;
+    [FieldOffset(0x268)] public AtkComponentNumericInput* AvgItemLevelInput;
+
+    [FieldOffset(0x270)] public AtkComponentButton* RecruitMembersButton;
+    [FieldOffset(0x278)] public AtkComponentButton* CancelButton;
+
+    [FieldOffset(0x280)] public AtkComponentDropDownList* DropDown3;
+    [FieldOffset(0x288)] public AtkComponentButton* ResetButton;
+
+    [FieldOffset(0x290)] public AtkComponentCheckBox* RemoveSpectatorsCheckBox;
+
+    [FieldOffset(0x298)] public AtkComponentButton* NormalButton;
+    [FieldOffset(0x2a0)] public AtkComponentButton* AllianceButton;
+    [FieldOffset(0x2a8)] public AtkComponentButton* CustomMatchButton;
+
+    [FieldOffset(0x2b0)] public AtkComponentDropDownList* DropDown4;
+    [FieldOffset(0x2b8)] public AtkComponentDropDownList* DropDown5;
+    [FieldOffset(0x2c0)] public AtkComponentDropDownList* DropDown6;
+    [FieldOffset(0x2c8)] public AtkComponentDropDownList* DropDown7;
+
+    [FieldOffset(0x318)] public AtkComponentCheckBox* CompletionStatusCheckBox;
+    [FieldOffset(0x340)] public AtkComponentCheckBox* UnrestrictedPartyCheckBox;
+    [FieldOffset(0x348)] public AtkComponentCheckBox* MinimumILCheckBox;
+    [FieldOffset(0x350)] public AtkComponentCheckBox* SilenceEchoCheckBox;
+}
--- TomestoneViewer/GameSystems/PartyFinderDetector.cs
+++ TomestoneViewer/GameSystems/PartyFinderDetector.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Linq;
 
+using Dalamud.Plugin.Services;
 using FFXIVClientStructs.FFXIV.Client.Game.Group;
 using FFXIVClientStructs.FFXIV.Client.UI;
 using FFXIVClientStructs.FFXIV.Client.UI.Info;
+using FFXIVClientStructs.FFXIV.Component.GUI;
 using Lumina.Excel.GeneratedSheets;
 
 namespace TomestoneViewer.GameSystems;
@@ -11,21 +13,34 @@ namespace TomestoneViewer.GameSystems;
 public unsafe class PartyFinderDetector
 {
     private readonly AddonInterface<AddonLookingForGroupDetail> partyFinderDetailsAddon;
+    private readonly AddonInterface<AddonLookingForGroupCondition> createPartyFinderAddon;
 
     public uint TerritoryId { get; private set; }
 
     public PartyFinderDetector()
     {
         this.partyFinderDetailsAddon = new("LookingForGroupDetail");
-        this.partyFinderDetailsAddon.AddOnEndHandler(this.OnAddonCloses);
+        this.partyFinderDetailsAddon.AddOnEndHandler(this.OnLookingForGroupDetailClose);
+
+        this.createPartyFinderAddon = new("LookingForGroupCondition");
+        this.createPartyFinderAddon.AddOnEndHandler(this.OnLookingForGroupConditionClose);
     }
 
     public void Dispose()
     {
         this.partyFinderDetailsAddon.Dispose();
+        this.createPartyFinderAddon.Dispose();
     }
 
-    private void OnAddonCloses(AddonLookingForGroupDetail* addon)
+    private void OnLookingForGroupConditionClose(AddonLookingForGroupCondition* addon)
+    {
+        var dutyName = SelectedItem(addon->DutyNameDropDown);
+
+        // can't be sure that pf was created, but this is good enough
+        this.UpdateTerritoryId(FindTerritoryId(dutyName));
+    }
+
+    private void OnLookingForGroupDetailClose(AddonLookingForGroupDetail* addon)
     {
         if (IsInParty())
         {
@@ -33,31 +48,34 @@ public unsafe class PartyFinderDetector
             var partyLeaderReal = new TeamList().Leader?.CharacterId?.FullName;
             if (partyLeaderAddon != partyLeaderReal)
             {
-                Service.PluginLog.Info("In party, ignoring addon close");
                 return;
-            }
-            else
-            {
-                Service.PluginLog.Info("Looking at your own party, update territory");
             }
         }
 
         var dutyName = StripNonAsci(addon->DutyNameTextNode->NodeText.ToString());
 
-        var targetTerritoryId = Service.DataManager.GetExcelSheet<ContentFinderCondition>()!
-             .FirstOrDefault(entry => string.Equals(
-                 entry.Name.ToString(),
-                 dutyName,
-                 StringComparison.OrdinalIgnoreCase))?
-                 .TerritoryType.Row;
+        // Can't be sure that party was joined, but this is a best effort
+        this.UpdateTerritoryId(FindTerritoryId(dutyName));
+    }
 
-        if (targetTerritoryId != null)
+    private void UpdateTerritoryId(uint? territoryId)
+    {
+        if (territoryId != null)
         {
             // Left commented log as a useful tool for detecting territoryId in future
             // Service.PluginLog.Info($"Closed dialog for territoryId={targetTerritoryId}");
-            // Can't be sure that party was joined, but this is a best effort
-            this.TerritoryId = targetTerritoryId.Value;
+            this.TerritoryId = territoryId.Value;
         }
+    }
+
+    private static uint? FindTerritoryId(string dutyName)
+    {
+        return Service.DataManager.GetExcelSheet<ContentFinderCondition>()!
+               .FirstOrDefault(entry => string.Equals(
+                     entry.Name.ToString(),
+                     dutyName,
+                     StringComparison.OrdinalIgnoreCase))?
+               .TerritoryType.Row;
     }
 
     private static string StripNonAsci(string input)
@@ -68,6 +86,12 @@ public unsafe class PartyFinderDetector
         }
 
         return input;
+    }
+
+    private static string SelectedItem(AtkComponentDropDownList* dropdown)
+    {
+        var selectedItem = dropdown->GetSelectedItemIndex();
+        return dropdown->List->GetItemRenderer(selectedItem)->AtkComponentButton.ButtonTextNode->NodeText.ToString();
     }
 
     private static bool IsInParty()
