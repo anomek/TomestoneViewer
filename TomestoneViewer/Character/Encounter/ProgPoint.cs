d28a664b1c42620eda23df74e01ec24676a6a06d
anomek <149334678+anomek@users.noreply.github.com>
2024-05-30 22:43:32 -0700

    Fix uwu p4. Udpate version to 0.1.3.2

--- TomestoneViewer/Character/Encounter/ProgPoint.cs
+++ TomestoneViewer/Character/Encounter/ProgPoint.cs
@@ -18,22 +18,24 @@ public record ProgPoint(IReadOnlyList<ProgPoint.Lockout> Lockouts)
     {
         public override string ToString()
         {
-            var prefix = this.Phase != null ? $"P{this.Phase} " : string.Empty;
-            return $"{prefix}{this.Number}%";
+            var prefix = this.Phase != null ? $"P{this.Phase}" : string.Empty;
+            var number = this.Number.HasValue ? $"{this.Number.Value}%" : string.Empty;
+            var space = prefix != string.Empty && number != string.Empty ? " " : string.Empty;
+            return $"{prefix}{space}{number}";
         }
 
         public string? Phase { get; private init; }
 
-        public int Number { get; private init; }
+        public int? Number { get; private init; }
 
         public static Percent From(string value)
         {
-            var number = int.Parse(value.Split('%')[0].Split('.')[0]);
+            var hasNumber = int.TryParse(value.Split('%')[0].Split('.')[0], out var number);
             var phases = value.Split('P');
             return new()
             {
                 Phase = phases.Length > 1 ? phases[1] : null,
-                Number = number,
+                Number = hasNumber ? number : null,
             };
         }
     }
