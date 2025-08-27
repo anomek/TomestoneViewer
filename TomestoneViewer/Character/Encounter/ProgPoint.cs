e278b927d6c5e8fab3dc5d1055e2b51abe148c3b
anomek <149334678+anomek@users.noreply.github.com>
2025-08-26 21:17:21 -0700

    Fixed parsing intermission phase

--- TomestoneViewer/Character/Encounter/ProgPoint.cs
+++ TomestoneViewer/Character/Encounter/ProgPoint.cs
@@ -18,7 +18,8 @@ public record ProgPoint(IReadOnlyList<ProgPoint.Lockout> Lockouts)
     {
         public override string ToString()
         {
-            var prefix = this.Phase != null ? $"P{this.Phase}" : string.Empty;
+            var prefix = this.Phase != null ? $"P{this.Phase}" : string.Empty
+                 + this.Intermission != null ? $"I{this.Intermission}" : string.Empty;
             var number = this.Number.HasValue ? $"{this.Number.Value}%" : string.Empty;
             var space = prefix != string.Empty && number != string.Empty ? " " : string.Empty;
             return $"{prefix}{space}{number}";
@@ -26,15 +27,19 @@ public record ProgPoint(IReadOnlyList<ProgPoint.Lockout> Lockouts)
 
         public string? Phase { get; private init; }
 
+        public string? Intermission { get; private init; }
+
         public int? Number { get; private init; }
 
         public static Percent From(string value)
         {
             var hasNumber = int.TryParse(value.Split('%')[0].Split('.')[0], out var number);
             var phases = value.Split('P');
+            var intermissions = value.Split('I');
             return new()
             {
                 Phase = phases.Length > 1 ? phases[1] : null,
+                Intermission = intermissions.Length > 1 ? intermissions[1] : null,
                 Number = hasNumber ? number : null,
             };
         }
